5814085822db551e8f9b01310e4d97350b2a29f8
farmer-Beixu <109709911+farmer-Beixu@users.noreply.github.com>
2024-03-30 03:24:23 +0800

    Add files via upload

---
+++ src/settings.b1d61.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default","UI2D"],collisionMatrix:[[false],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"4188c",main:"cd660"}};
